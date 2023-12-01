586d8a3aded4b58863f348bc228d9a07b122d70d
michaelschoellhorn <m-schoellhorn@gmx.de>
2023-12-01 09:46:33 +0100

    Remove redundant flux test in main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 
 int main()
 {
-    double f = flux(fromm, 10.0, 9.0, 8.0, 7.0, 1.0, 0.1, 0.2);
-    std::cout
-        << f << std::endl;
-    // Mat QOne({std::vector<double>({1.5, 2.0, 3.5, 4.0}), std::vector<double>({1.0, 2.0, 3.0, 4.0}), std::vector<double>({5.0, 6.0, 7.0, 8.0}), std::vector<double>({5.0, 6.0, 7.0, 8.0})});
     Mat QOne(40, std::vector<double>(40, 1.0));
     for (size_t i = 0; i < 20; i++)
     {
